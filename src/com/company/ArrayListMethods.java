8fdfa28385bbc46b522ee00f072aae4e5ea10ea3
richaar <arianna.richardson@ssfs.org>
2020-02-21 09:00:03 -0500

    Started ArrayList

--- src/com/company/ArrayListMethods.java
+++ src/com/company/ArrayListMethods.java
@@ -24,8 +24,7 @@ return list;
         double num =0;
         ArrayList<Double> list = new ArrayList<Double>();
         while (num != 0)
-        {
-            num = input.nextDouble();
+        { num = input.nextDouble();
             list.add(num);
     }
 
