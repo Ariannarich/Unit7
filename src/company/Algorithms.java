013b0ca5ddaf0bbf14f4609d56dcda38b5547c48
richaar <arianna.richardson@ssfs.org>
2020-03-05 11:19:44 -0500

    Started algorithms

--- src/company/Algorithms.java
+++ src/company/Algorithms.java
@@ -27,21 +27,50 @@ public class Algorithms {
         }
         return list3;
     }
-        public static ArrayList<String> fileDuplicatesTwo() throws IOException
-        {
 
-            Scanner input = new Scanner(new File("names.txt"));
-            ArrayList<String> list1 = new ArrayList<>();
+    public static ArrayList<String> fileDuplicatesTwo() throws IOException {
 
-            while(input.hasNext())
-            {
-                list1.add(input.next());
-            }
-            int count = 0;
+        Scanner input = new Scanner(new File("names.txt"));
+        ArrayList<String> list = new ArrayList<>();
 
-            }
-
+        while (input.hasNext()) {
+            list.add(input.next());
         }
+        for(int y = 0; y< list.size() -1; y++)
+        {
+        for (int x = list.size() -1; x >= 0; x--) {
+            if (list.get(x).equals(list.get(y))) {
+                list.remove(x);
+            }
+        }
+        }
+        return list;
 
     }
-}
+
+    public static ArrayList<Integer> orderedList()throws IOException
+    {
+
+        Scanner input = new Scanner(new File("file1.txt"));
+        ArrayList<Integer> numbers = new ArrayList<>();
+
+        while(input.hasNext())
+        {
+            numbers.add(input.nextInt());
+        }
+        for(int i = numbers.size() -2; i >=0; i--)
+        {
+            if (numbers. get(i) > numbers.get(i+1))
+            {
+                int temp = numbers.get(i);
+                numbers.remove(numbers.get(i));
+                numbers.add(temp);
+            }
+        }
+        return numbers;
+    }
+
+    }
+
+
+
