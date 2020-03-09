d1191cbc169c27b333510133b0e45199d064eda1
arian <arianna.richardson@ssfs.org>
2020-03-09 13:29:16 -0400

    Finished Traversing

--- src/company/Traversing.java
+++ src/company/Traversing.java
@@ -6,25 +6,23 @@ import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Traversing {
-    public static Scanner input = new Scanner(System.in);
 
     public static ArrayList<String> getStates() {
 
+        Scanner input = new Scanner(System.in);
         ArrayList<String> list = new ArrayList<String>();
-        for (int i = list.size(); i >=0 ; i--) {
+        for (int i = 0 ; i < list.size() ; i++) {
             System.out.println("Next state or \"Stop\":");
             String word = input.next();
             list.add(word);
             if (word.equals("Stop")) {
-                while (list.get(i).equals("Stop")) {
-                    System.out.println("Next state or \"Stop\":");
-                    word = input.next();
-                    list.add(word);
+                list.remove(i);
+                break;
+
                 }
             }
 
-        }
-        return list;
+                return list;
     }
 
     public static String createList(ArrayList<String> states) {
